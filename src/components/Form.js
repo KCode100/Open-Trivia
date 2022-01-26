154fa52e01f4c62434e97140457f278b6dca7ee4
Kivi Corn <magiciankivi@gmail.com>
2022-01-26 17:49:46 +0000

    style main quiz. add sekelton loader.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { categories } from "../data/categories";
 import Quiz from "./Quiz";
+import Loading from "./Loading"
 
 // material ui
-import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
 import TextField from '@mui/material/TextField';
-import { CardContent, Container, Stack } from "@mui/material";
+import { Container, Stack } from "@mui/material";
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -15,6 +15,7 @@ import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import * as React from 'react';
 
 const Form = () => {
     const [numberOfQuestions, setNumberOfQuestions] = useState(null)
@@ -84,85 +85,71 @@ const Form = () => {
     return (
         <>
             {!quizData && !loading &&
-                <Container maxWidth="xl">
-                    <Stack direction="row" flexWrap="wrap-reverse" gap={4}>
-                        <Paper elevation={3}>
-                            <Box p={4}>
-                                <Typography variant="h3" component="h2" gutterBottom>
-                                    Let's get started
-                                </Typography>
-                                <form onSubmit={formHandler}>
-                                    <Stack spacing={3}>
-                                        <TextField type="number" name="number" inputProps={{ min: 1, max: 30 }} defaultValue={10} label="Number of Questions" fullWidth />
-                                        <FormControl fullWidth>
-                                            <InputLabel id="category">Category</InputLabel>
-                                            <Select
-                                                labelId="category"
-                                                id="category"
-                                                value={category}
-                                                label="category"
-                                                onChange={handleCategory}
-                                            >
-                                                {categories.map(category => (
-                                                    <MenuItem value={category.id} key={category.id}>{category.name}</MenuItem>
-                                                ))}
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl fullWidth>
-                                            <InputLabel id="difficulty">Difficulty</InputLabel>
-                                            <Select
-                                                labelId="difficulty"
-                                                id="difficulty"
-                                                value={difficulty}
-                                                label="Difficulty"
-                                                onChange={handleDifficulty}
-                                            >
-                                                <MenuItem value={""}>Any difficulty</MenuItem>
-                                                <MenuItem value={"easy"}>Easy</MenuItem>
-                                                <MenuItem value={"medium"}>Medium</MenuItem>
-                                                <MenuItem value={"hard"}>Hard</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl fullWidth>
-                                            <InputLabel id="type">Type</InputLabel>
-                                            <Select
-                                                labelId="type"
-                                                id="type"
-                                                value={quizType}
-                                                label="type"
-                                                onChange={handleQuizType}
-                                            >
-                                                <MenuItem value={""}>Any type</MenuItem>
-                                                <MenuItem value={"multiple"}>Multiple Choice</MenuItem>
-                                                <MenuItem value={"boolean"}>True / False</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <Button variant="contained" type="submit" fullWidth>
-                                            Start Quiz
-                                        </Button>
-                                        {error && <Alert severity="error">{error}</Alert>}
-                                    </Stack>
-                                </form>
-                            </Box>
-                        </Paper>
-                        <Box pt={3} justifyContent="center">
+                <Container maxWidth="md">
+                    {/* <Stack direction="row" flexWrap="wrap-reverse" gap={4}> */}
+                    <Paper elevation={3}>
+                        <Box p={4}>
                             <Typography variant="h3" component="h2" gutterBottom>
-                                other side
+                                Let's get started
                             </Typography>
+                            <form onSubmit={formHandler}>
+                                <Stack spacing={3}>
+                                    <TextField type="number" name="number" inputProps={{ min: 1, max: 30 }} defaultValue={10} label="Number of Questions" fullWidth />
+                                    <FormControl fullWidth>
+                                        <InputLabel id="category">Category</InputLabel>
+                                        <Select
+                                            labelId="category"
+                                            id="category"
+                                            value={category}
+                                            label="category"
+                                            onChange={handleCategory}
+                                        >
+                                            {categories.map(category => (
+                                                <MenuItem value={category.id} key={category.id}>{category.name}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="difficulty">Difficulty</InputLabel>
+                                        <Select
+                                            labelId="difficulty"
+                                            id="difficulty"
+                                            value={difficulty}
+                                            label="Difficulty"
+                                            onChange={handleDifficulty}
+                                        >
+                                            <MenuItem value={""}>Any difficulty</MenuItem>
+                                            <MenuItem value={"easy"}>Easy</MenuItem>
+                                            <MenuItem value={"medium"}>Medium</MenuItem>
+                                            <MenuItem value={"hard"}>Hard</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="type">Type</InputLabel>
+                                        <Select
+                                            labelId="type"
+                                            id="type"
+                                            value={quizType}
+                                            label="type"
+                                            onChange={handleQuizType}
+                                        >
+                                            <MenuItem value={""}>Any type</MenuItem>
+                                            <MenuItem value={"multiple"}>Multiple Choice</MenuItem>
+                                            <MenuItem value={"boolean"}>True / False</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <Button variant="contained" size="large" type="submit" fullWidth>
+                                        Start Quiz
+                                    </Button>
+                                    {error && <Alert severity="error">{error}</Alert>}
+                                </Stack>
+                            </form>
                         </Box>
-                    </Stack>
+                    </Paper>
                 </Container>
             }
-            {
-                loading &&
-                <Box sx={{ display: 'flex' }}>
-                    <CircularProgress />
-                </Box>
-            }
-            {
-                quizData &&
-                <Quiz quizData={quizData} reset={reset} />
-            }
+            {loading && <Loading />}
+            {quizData && <Quiz quizData={quizData} reset={reset} />}
         </>
     );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import LinearProgress from '@mui/material/LinearProgress';
+import { Container, Grid, Stack } from "@mui/material";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Quiz = ({ quizData, reset }) => {
     const [currentQuestion, setCurrentQuestion] = useState(0)
@@ -17,28 +26,102 @@ const Quiz = ({ quizData, reset }) => {
         // }, 1500);
     }
 
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <>
-            <LinearProgress variant="determinate" value={percentage} />
-            {currentQuestion < quizData.length &&
+            <Container maxWidth="lg">
+                {currentQuestion < quizData.length &&
+                    <>
+                        <Grid container spacing={{ xs: 2, md: 3 }} sx={{
+                            paddingBottom: {
+                                xs: 4,
+                                md: 8
+                            }
+                        }}>
+                            <Grid item item xs={12} sm={7}>
+                                <Typography variant="h6" component="div" gutterBottom>
+                                    {quizData[currentQuestion].category}
+                                </Typography>
+                            </Grid>
+                            <Grid item item xs={12} sm={3}>
+                                <Typography variant="h6" component="div" gutterBottom>
+                                    Difficulty: {quizData[currentQuestion].difficulty}
+                                </Typography>
+                            </Grid>
+                            <Grid item item xs={12} sm={2}>
+                                <Typography variant="h6" component="div">Score: {score}</Typography>
+                            </Grid>
+                        </Grid>
+                        <LinearProgress variant="determinate" value={percentage} />
+                        <Typography pt={2} variant="h4" component="h2">
+                            <span dangerouslySetInnerHTML={{ __html: quizData[currentQuestion].question }} />
+                        </Typography>
+                        <Stack sx={{
+                            paddingTop: {
+                                xs: 4,
+                                md: 8
+                            }
+                        }} spacing={{ xs: 1, sm: 2, md: 4 }} direction={{ xs: 'column', sm: 'row' }}>
+                            {quizData[currentQuestion].choices.map(choice => (
+                                <Button variant="contained" size="large" onClick={() => answerHandler(choice)}
+                                    key={choice}>
+                                    <span
+                                        dangerouslySetInnerHTML={{ __html: choice }}
+                                    />
+                                </Button>
+                            ))}
+                        </Stack>
+                        <Box sx={{
+                            paddingTop: {
+                                xs: 4,
+                                md: 8
+                            }
+                        }}>
+                            <Button variant="outlined" onClick={handleClickOpen}>
+                                Quit
+                            </Button>
+                        </Box>
+                    </>
+                }
+                {currentQuestion >= quizData.length &&
+                    <div className="game-over">
+                        <span>{score}/ {quizData.length}</span>
+                        <Button variant="contained" size="large" onClick={reset}>Play again</Button>
+                    </div>
+                }
                 <div>
-                    <h2>{quizData[currentQuestion].category}</h2>
-                    <h3 dangerouslySetInnerHTML={{ __html: quizData[currentQuestion].question }} />
-                    <p>Difficulty: {quizData[currentQuestion].difficulty}</p>
-                    {quizData[currentQuestion].choices.map(choice => (
-                        <button
-                            dangerouslySetInnerHTML={{ __html: choice }}
-                            onClick={() => answerHandler(choice)}
-                            key={choice}
-                        />
-                    ))}
+                    <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            {"Quit game?"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                Your current score will NOT be saved
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Disagree</Button>
+                            <Button onClick={reset} autoFocus>
+                                Agree
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
-            }
-            <div>Score: {score} / {quizData.length}</div>
-            {currentQuestion >= quizData.length &&
-                <button onClick={reset}>Replay</button>
-            }
-            <button>Restart (material ui alert here...)</button>
+            </Container>
         </>
     );
 }
